4671e9797053bef9c6341440856b7f1f2fdc2a04
7810 <59080396+7810@users.noreply.github.com>
2020-01-24 08:53:04 +0100

    Add files via upload
    le sketch de la grille

---
+++ sketch grille.js
@@ -0,0 +1,15 @@
+function setup() {
+   createCanvas(520, 700);
+  background('rgb(115, 194, 251)');
+  rect(50,150,400,350);
+ // fond blanc de la grille
+  for(var h = 50; h<500; h= h+50){
+    //h est le nombre qui sépare les lignes horizontales
+    line(50, 150 +h ,450, 150+h);
+  }
+  for (var v = 50; v<450; v= v+50){
+  //v est le nombre qui sépare les lignes verticales
+    line(50 +v , 150, 50 +v, 500);
+  }
+    //créaction de la grille de 8 colones et 7 lignes + d'un fond blanc derrière la grille
+}
