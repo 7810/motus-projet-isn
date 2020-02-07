69d49b0d6603210e159fba09c872ec4181efe5a1
Lecharisme <59723696+Lecharisme@users.noreply.github.com>
2020-02-07 09:23:09 +0100

    Add files via upload

---
+++ affichageappeler.js
@@ -0,0 +1,3 @@
+traitement_couleurs(mot_ordinateur,mot_joueur)
+affichage(mot_joueur,compteur_essais, traitement_couleurs)
+compteur_essais = compteur_essais + 1 // permet de passer une ligne à chaque clic sur OK
