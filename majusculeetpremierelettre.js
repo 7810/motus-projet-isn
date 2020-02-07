a67ef42f38e613c61009b5b9668481b42ce054c7
Lecharisme <59723696+Lecharisme@users.noreply.github.com>
2020-02-07 08:47:41 +0100

    Add files via upload

---
+++ majusculeetpremierelettre.js
@@ -0,0 +1,7 @@
+  mot_ordinateur.toUpperCase();
+  // permet de passer le mot à trouver en majuscule
+  premierCaractere = mot_ordinateur.charAt(0).toUpperCase();
+  textSize(15);
+  text(premierCaractere,380,145);
+  text('La première lettre du mot est un :',140,145);
+// la première lettre du mot de l'ordinateur est inscrite dans la première case de la grille
