c910a45e98cb00cc6fb9dfa040bb4620c92ff317
Aekino <59080519+Aekino@users.noreply.github.com>
2020-02-07 08:14:19 +0100

    version alan

--- sketch.js
+++ sketch.js
@@ -1,39 +1,43 @@
 //variables globales
-var mot;
-var mot_joueur; 
+var zone_mot;
+var mot_joueur;
 var mot_ordinateur;
-var i=150; //i: lignes (coordonées y)
 var compteur_essais = 0; // compteur permettant de changer de ligne
+var T1 = []
+var T2 = []
+var i = 0
+var j = 0
+var D = 0
 
 function setup() { //menu du jeu
   createCanvas(520, 700);
-  background('rgb(115, 194, 251)')
-  textSize(20)
-  text('Mode classique :',50, 200)
-  text('Mode illimité :',300,200)
-  textSize(60)
-  text('MOTUS',150,70)
-  textStyle(BOLD)
-  var facile = createButton('Facile')
-  var normal= createButton('Normal')
-  var difficile =createButton('Difficile')
-  facile.position(75, 220)
-  normal.position(75, 300)
-  difficile.position(75,380)
-  facile.mousePressed(interface_facile)
-  normal.mousePressed(interface_normale)
-  difficile.mousePressed(interface_difficile)
-  facile.size(100,50)
-  normal.size(100,50)
-  difficile.size(100,50)
-  facile.style('background-color',color(46, 201, 122))
-  normal.style('background-color',color(236, 170, 0))
-  difficile.style('background-color', color(236, 0, 0))
-  var mode_illlimité = createButton('Mode illimité')
-  mode_illlimité.position(315, 300)
-  mode_illlimité.mousePressed(interface_illimité)
-  mode_illlimité.size(100,50)
-  mode_illlimité.style('background-color',color(236, 170, 0))
+  background('rgb(115, 194, 251)');
+  textSize(20);
+  text('Mode classique :',50, 200);
+  text('Mode illimité :',300,200);
+  textSize(60);
+  textStyle(BOLD);
+  text('MOTUS',150,70);
+  var facile = createButton('Facile');
+  var normal = createButton('Normal');
+  var difficile = createButton('Difficile');
+  facile.position(75, 220);
+  normal.position(75, 300);
+  difficile.position(75,380);
+  facile.mousePressed(interface_facile);
+  normal.mousePressed(interface_normale);
+  difficile.mousePressed(interface_difficile);
+  facile.size(100,50);
+  normal.size(100,50);
+  difficile.size(100,50);
+  facile.style('background-color',color(46, 201, 122));
+  normal.style('background-color',color(236, 170, 0));
+  difficile.style('background-color', color(236, 0, 0));
+  var mode_illimité = createButton('Mode illimité');
+  mode_illimité.position(315, 300);
+  mode_illimité.mousePressed(interface_illimité);
+  mode_illimité.size(100,50);
+  mode_illimité.style('background-color',color(236, 170, 0));
 
 }
  
@@ -42,74 +46,71 @@ function interface_facile(){}
 function interface_normale(){
   background('rgb(115, 194, 251)');
   rect(50,150,400,350);
- // fond blanc de la grille
-  for(var h = 50; h<400; h= h+50){
-    //h est le nombre qui sépare les lignes horizontales
+  //fond blanc de la grille
+  for (var h = 0; h<=350; h= h+50) {
+  //h est le nombre qui sépare les lignes horizontales
     line(50, 150 +h ,450, 150+h);
   }
-  for (var v = 50; v<450; v= v+50){
+  for (var v = 0; v<=400; v= v+50) {
   //v est le nombre qui sépare les lignes verticales
     line(50 +v , 150, 50 +v, 500);
   }
-    //créaction de la grille de 8 colones et 7 lignes (carré 50*50) + d'un fond blanc derrière la grille
+    //création de la grille de 8 colones et 7 lignes (carré 50*50) + d'un fond blanc derrière la grille
   
  var tab_A = ['accoudez', 'aggraver','alléluia','baladeur','bestiole','boulange','cacaoyer','catcheur','costumes','descente','délégués','dînettes','embossés','exclamez','évoquant','farfadet','foreuses','féodales','galloise','goupille','gêneuses','handball','humecter','héroïque','immature','inhumain','ivrognes','jaugeait','jonchant','jurerons','kakatoès','klaxonna','kérosène','lamproie','listings','lézardes','malaxeur','mensuels','méprisée','naviguât','novation','névrosés','obsèques','oncogène','oxygéner','paladins','pivotant','pénibles','quarrant','quignons','quêteurs','racismes','renfoncé','révision','salariat','soutenez','sûrement','tamiseur','toperons','tôleries','ulcèrent','uranisme','usurpées','vaisseau','violines','vétérans','wagonnée','wallonne','witloofs','xanthine','ximénies','xylidine','yachtman','yttriums','yearling','zieutais','zoologie','zozotait'] ;
-  mot=createInput();
+  zone_mot=createInput();
   //variable disponible dans toutes les fonctions
-  mot.position(150,100);
-  mot.size(200);
+  zone_mot.position(150,100);
+  zone_mot.size(200);
   textSize(60);
-  text('MOTUS',150,70);
   textStyle(BOLD);
+  text('MOTUS',150,70);
   textSize(15);
   text('⚠ Attention le mot choisit doit être de 8 lettres ⚠',85,550);
   var valider=createButton('OK');
   valider.position(365,100);
   valider.mousePressed(commencer_normal);
   mot_ordinateur=random(tab_A);
+  print (mot_ordinateur);
   //selectionne aléatoirement un mot du tableau A qui va ensuite s'afficher dans la grille
-   mot_ordinateur.toUpperCase()
-  premierCaractere = mot_ordinateur.charAt(0).toUpperCase()
-    textSize(20)
-  text(premierCaractere, 68,180)
+  mot_ordinateur.toUpperCase();
+  premierCaractere = mot_ordinateur.charAt(0).toUpperCase();
+  textSize(20);
+  text(premierCaractere,68,180);
 // la première lettre du mot de l'ordinateur est inscrite dans la première case de la grille
   
-
-  
-  
   textSize(20);
-  text('Lettres en            : sont dans le mot et bien placées', 30,600);
+  text('Lettres en             : sont dans le mot et bien placées', 10,600);
   fill(0, 160, 0);
-  text('vertes',130,600);
-  fill(0,0,0)
+  text('vertes',115,600);
+  fill(0,0,0);
   text('Lettres en             : sont dans le mot mais ne sont pas', 10, 620);
   fill(255, 200, 0);
   text('jaunes',115,620);
   fill(0,0,0);
-  text(' bien placées',230,640);
-  text('Lettres non colorées : ne sont pas dans le mot',40,660);
+  text('bien placées',190,640);
+  text('Lettres non colorées : ne sont pas dans le mot',10,660);
   stroke(0, 160, 0);
-  line(135,602,190,602);
+  line(115,602,175,602);
   stroke(255, 200, 0);
-  line(120,622,175,622);
+  line(115,622,175,622);
 }
-
 
 function commencer_normal(){
 //fonction prenant en compte le mot choisit et validé par le joueur  
-  mot_joueur= mot.value();
+  mot_joueur= zone_mot.value();
   print (mot_joueur);
   //mot choisit par le joueur dans la zone de texte
   if(mot_joueur.length != 8){ //quand le mot n'a pas 8 lettres, message d'erreur
-    mot.value('');
+  zone_mot.value('');
   alert('Veuillez entrer un mot de 8 lettres');
   }
   else{
     
   
-    if(mot_joueur == '' /*comporte un caractère interdit*/){
-  mot.value('')
-    alert('Veuillez ne pas entrer des caractères autres que des lettres')
+  if(mot_joueur == '' /*comporte un caractère interdit*/){
+  zone_mot.value('')
+  alert('Veuillez ne pas entrer des caractères autres que des lettres')
   }
   
 
@@ -120,22 +121,66 @@ function commencer_normal(){
   
 
 }
-  
+traitement_couleurs(mot_ordinateur,mot_joueur)
 affichage(mot_joueur,compteur_essais)
-    compteur_essais = compteur_essais + 1 // permet de passer une ligne à chaque clic sur OK
-  }  
+compteur_essais = compteur_essais + 1 // permet de passer une ligne à chaque clic sur OK
+  }
+  
+}
+
+function traitement_couleurs(mot_ordinateur,mot_joueur) { //fonction permettant de traiter les couleurs selon les lettres saisies par le joueur.
+  
+  //Tableau avec le mot à trouver.
+  for (i=0; i<=7; i++) {
+  T1[i]=mot_ordinateur.charAt(i);     //position du caractere du mot_ordinateur
+  }
+  
+  //Tableau avec le mot saisie par le joueur.
+  for (i=0; i<=7; i++) {
+  T2[i]=mot_joueur.charAt(i);        //position du caractere du mot_joueur
+  }
+  
+  //Algorithme de traitement des couleurs selon les lettres saisies par le joueur.
+  for (i=0; i<=7; i++) {
+    
+  //Partie pour définir la couleur des lettres bien placées (lettres en vertes).
+  if (T2[i]==T1[i]) {         //si la position du caractere du mot_joueur correspond à la position du caractere du mot_ordinateur alors la lettre est verte.
+  fill(0, 160, 0);
+  text(T2[i]);
+  }
+    
+   //Partie pour definir la couleur des lettres qui sont dans le mot mais au mauvais endroit (lettres en jaunes).
+  for (j=0; j<=7; j++) {
+  if (T2[i]==T1[j] && j!=i) {  //si la lettre est dans le mot mais au mauvais endroit alors la lettre est jaune.
+  fill(255, 200, 0);
+  text(T2[i]);
+  }
+  }
+    
+  //Partie pour définir la couleur des lettres mauvaises (lettres non colorées).
+  for (j=0; j<=7; j++) {
+  if (T2[i]!=T1[j]) {
+  D=D+1;
+  }
+    
+  if (D==8) { //compteur D pour vérifier que la lettre selectionée est bien   différente de toute les autres.
+  fill(0,0,0);
+  text(T2[i]);
+  }
+  }
+  }
 }
 
 function interface_difficile(){}
 function interface_illimité(){}
 
-function affichage(mot_ordinateur,ligne){
+function affichage(mot_ordinateur,compteur_essais){
   
 
 
   for(var colonne = 1 ; colonne<=7; colonne=colonne+1){ //colonne est le nombre de caractères
   // quand un essai est fait, passer à la ligne suivante de la grille et stocker les caractères
-   text(mot_ordinateur[colonne],68 + 50*colonne, 180+ligne*50) 
+   text(mot_ordinateur[colonne],68 + 50*colonne, 180+compteur_essais*50) 
     
     
   }
@@ -143,4 +188,3 @@ function affichage(mot_ordinateur,ligne){
   
   
 }
-
