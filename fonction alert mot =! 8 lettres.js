09b7b7e60fbb1e9ffe2d8325aa6bf2ceb31dcaa3
7810 <59080396+7810@users.noreply.github.com>
2020-02-07 08:50:19 +0100

    Add files via upload
    fonction qui donne une alerte quand le mot du joueur est différent de 8 lettres

--- fonction alert mot =! 8 lettres.js
+++ fonction alert mot =! 8 lettres.js
@@ -1,7 +1,10 @@
 function setup() {
   createCanvas(400, 400);
-   if(mot_joueur.length != 8){ //quand le mot n'a pas 8 lettres, message d'erreur
-    mot.value('');
+  mot_joueur= zone_mot.value();
+  print (mot_joueur);
+  //mot choisit par le joueur dans la zone de texte
+  if(mot_joueur.length != 8){ //quand le mot n'a pas 8 lettres, message d'erreur
+    zone_mot.value('');
   alert('Veuillez entrer un mot de 8 lettres');
   }
 }
