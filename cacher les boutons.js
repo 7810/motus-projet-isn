a99e2e8428f56451760531202edc10c94692654d
7810 <59080396+7810@users.noreply.github.com>
2020-02-07 08:29:28 +0100

    Add files via upload
    pour cacher les boutons à intégrer au début d'une interface de jeu

---
+++ cacher les boutons.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400, 400);
+  facile.style('visibility','hidden');
+  normal.style('visibility','hidden');
+  difficile.style('visibility','hidden');
+  background('rgb(115, 194, 251)');
+  // à intégrer au début 
+}
+
