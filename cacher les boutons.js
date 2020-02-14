f9653275de82f5552b1cde31d895ee1579e5fc4f
7810 <59080396+7810@users.noreply.github.com>
2020-02-14 08:24:14 +0100

    Add files via upload
    pour cacher les boutons avec le bouton mode score

--- cacher les boutons.js
+++ cacher les boutons.js
@@ -1,9 +1,8 @@
-function setup() {
-  createCanvas(400, 400);
   facile.style('visibility','hidden');
   normal.style('visibility','hidden');
   difficile.style('visibility','hidden');
+  mode_score.style('visibility', 'hidden');
   background('rgb(115, 194, 251)');
-  // à intégrer au début 
-}
+  // à intégrer au début d'uneinterface de jeu  
+
 
