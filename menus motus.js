d6cd190fca89697f70ccbb5a629dce9cb0fc3ff3
7810 <59080396+7810@users.noreply.github.com>
2020-02-14 08:18:26 +0100

    Add files via upload
    menus du motus avec le mode score

--- menus motus.js
+++ menus motus.js
@@ -1,4 +1,4 @@
-function setup() {
+function setup() { //menu du jeu
   createCanvas(520, 700);
   background('rgb(115, 194, 251)');
   textSize(30);
@@ -11,7 +11,7 @@ function setup() {
   difficile =createButton('Difficile');
   facile.position(225, 250);
   normal.position(225, 330);
-  difficile.position(225,420);
+  difficile.position(225,410);
   facile.mousePressed(interface_facile);
   normal.mousePressed(interface_normale);
   difficile.mousePressed(interface_difficile);
@@ -21,9 +21,9 @@ function setup() {
   facile.style('background-color',color(46, 201, 122));
   normal.style('background-color',color(236, 170, 0));
   difficile.style('background-color', color(236, 0, 0));
-}
-}
-
-function draw() {
-  background(220);
+  mode_score= createButton('Mode score')
+  mode_score.position(225, 550)
+  mode_score.mousePressed(modescore)
+  mode_score.size(100,50)
+  mode_score.style('background-color', color(110, 11, 20))
 }
